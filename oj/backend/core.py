c58c2debd80476fc8b3d902848b1420b3c714cfb
hoshizora-d <hoshizora.d@icloud.com>
2020-10-22 22:12:28 +0900

    update test time usage feature

--- oj/backend/core.py
+++ oj/backend/core.py
@@ -1,7 +1,7 @@
 import os
 import time
+from concurrent.futures import ThreadPoolExecutor
 from typing import List, Tuple, Dict, Any
-from joblib import Parallel, delayed
 from oj.backend.data import JsonDataGenerator
 
 
@@ -12,14 +12,19 @@ class Problem:
     __time_limit__ = 1000
 
     def test_all(self):
+        def test_case_callback(future):
+            future.end_time = time.process_time()
+
         max_time = 0
-        for args, kwargs, expected in self.generate_cases():
-            task = delayed(self.solve)(*args, **kwargs)
-            now = time.process_time()
-            answer = Parallel(-1, timeout=self.__time_limit__/1000)([task])[0]
-            assert self.judge(expected, answer)
-            max_time = max(time.process_time() - now, max_time)
-        print(f"time: {max_time*1000:.0f}ms")
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            for args, kwargs, expected in self.generate_cases():
+                future = executor.submit(self.solve, *args, **kwargs)
+                future.add_done_callback(test_case_callback)
+                start_time = time.process_time()
+                answer = future.result(self.__time_limit__/1000)
+                assert self.judge(expected, answer)
+                max_time = max(future.end_time - start_time, max_time)
+            print(f"time: {max_time*1000:.0f}ms")
 
     def generate_cases(self) -> Tuple[List, Dict, Any]:
         root = os.path.join(self.__data__, self.__title__)
